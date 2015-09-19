bb6606cbfc35d1e1b19e956a7f89d1677d79d85d
Nick Renford <nickrenfo@gmail.com>
2015-09-19 13:52:27 -0500

    add headers to ajax call

--- smite/doSmite.js
+++ smite/doSmite.js
@@ -4,8 +4,9 @@ $(function() {
     $.ajax({
         url:getQueryURL("createsession"),
         crossDomain: true,
-        json: "json_callback",
-        dataType:"jsonp"
+        jsonp: "json_callback",
+        dataType:"jsonp",
+        headers:{"Access-Control-Allow-Origin":"api.smitegame.com"}
     }).done(function(response){
         signature = response.session_id;
         console.log(signature);
